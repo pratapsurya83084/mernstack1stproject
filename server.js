7989041ece24ced66603e8dfa62b0bc135bbfbec
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-01-30 23:22:58 +0530

    add cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // CORS setup for allowing the frontend to communicate with the backend
 app.use(
   cors({
-    origin: "https://mernwebapplication.netlify.app/", // Replace with your frontend URL
+    origin: "http://localhost:5173", // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // To send credentials (cookies, tokens)
     allowedHeaders: ["Content-Type", "Auth"],
