8b1f4781f293a0c5dd2e0eab82029ff4a9663ff8
Pratap Suryawanshi <pratapsuryawanshi007@gmail.com>
2025-05-19 20:38:22 +0530

    add furl

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin:"http://localhost:5173",
-    origin: "https://ecom-mernapplication.netlify.app/",
+    origin: "https://mernapplication.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // Allow cookies and tokens
     // allowedHeaders: ["Content-Type", "Auth"],  // Use "Authorization" for authentication
